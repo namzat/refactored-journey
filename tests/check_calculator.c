cfe8e051207c522355d6e67958e0199a6e54b575
Chris Bernholt <cbernhlt@hotmail.com>
2016-06-17 12:47:16 -0400

    Second test failing

--- tests/check_calculator.c
+++ tests/check_calculator.c
@@ -6,6 +6,10 @@ START_TEST(I_plus_I_returns_II) {
     ck_assert_str_eq(add("I", "I"), "II");
 } END_TEST
 
+START_TEST(I_plus_II_returns_III) {
+    ck_assert_str_eq(add("I", "I"), "III");
+} END_TEST
+
 Suite * calculator_suite(void) {
     Suite *s;
     TCase *tc_core;
@@ -16,6 +20,8 @@ Suite * calculator_suite(void) {
     tc_core = tcase_create("Core");
 
     tcase_add_test(tc_core, I_plus_I_returns_II);
+    tcase_add_test(tc_core, I_plus_II_returns_III);
+
     suite_add_tcase(s, tc_core);
 
     return s;
