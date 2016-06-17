cfdbd10950fb4f6cdb4a13a12235ce11e9a0f2fc
Chris Bernholt <cbernhlt@hotmail.com>
2016-06-17 11:04:16 -0400

    failing test - add function missing

--- tests/check_calculator.c
+++ tests/check_calculator.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "check_calculator.h"
 
+START_TEST(I_plus_I_returns_II) {
+    ck_assert_str_eq(add("I", "I"), "II");
+} END_TEST
+
 Suite * calculator_suite(void) {
     Suite *s;
     TCase *tc_core;
@@ -10,6 +14,7 @@ Suite * calculator_suite(void) {
     /* Core test case */
     tc_core = tcase_create("Core");
 
+    tcase_add_test(tc_core, I_plus_I_returns_II);
     suite_add_tcase(s, tc_core);
 
     return s;
