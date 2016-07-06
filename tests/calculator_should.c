5de08f66646d9e89a9c016cec5f9c6450e6aa55b
Chris Bernholt <cbernhlt@hotmail.com>
2016-07-06 12:10:00 -0400

    refactor

--- tests/calculator_should.c
+++ tests/calculator_should.c
@@ -19,14 +19,6 @@ START_TEST(calculate_I_plus_II_as_III) {
     ck_assert_str_eq("III", roman_total);
 } END_TEST
 
-START_TEST(return_status_error_if_passed_null_value) {
-    char *null_argument = 0;
-    char roman_total[MAX_ROMAN_NUMERAL_STRING_SIZE];
-    ck_assert_int_eq(EXIT_FAILURE, add(roman_total, null_argument, "I"));
-    ck_assert_int_eq(EXIT_FAILURE, add(roman_total, "I", null_argument));
-    ck_assert_int_eq(EXIT_FAILURE, add(null_argument, "I", "I"));
-} END_TEST
-
 START_TEST(calculate_V_minus_I_as_IV) {
     char roman_total[MAX_ROMAN_NUMERAL_STRING_SIZE] = {'\0'};
     ck_assert_int_eq(EXIT_SUCCESS, subtract(roman_total, "V", "I"));
@@ -37,6 +29,14 @@ START_TEST(calculate_X_minus_II_as_VIII) {
     char roman_total[MAX_ROMAN_NUMERAL_STRING_SIZE] = {'\0'};
     ck_assert_int_eq(EXIT_SUCCESS, subtract(roman_total, "X", "II"));
     ck_assert_str_eq("VIII", roman_total);
+} END_TEST
+
+START_TEST(return_status_error_if_passed_null_value) {
+    char *null_argument = 0;
+    char roman_total[MAX_ROMAN_NUMERAL_STRING_SIZE];
+    ck_assert_int_eq(EXIT_FAILURE, add(roman_total, null_argument, "I"));
+    ck_assert_int_eq(EXIT_FAILURE, add(roman_total, "I", null_argument));
+    ck_assert_int_eq(EXIT_FAILURE, add(null_argument, "I", "I"));
 } END_TEST
 
 Suite * calculator_suite(void) {
